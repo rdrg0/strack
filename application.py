d8225a9749adf62d871012f71f1f3dc7f12cbf2b
rdrg0 <rodrigo.arriaran@gmail.com>
2020-12-17 19:19:58 -0500

    fixing errors

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import psycopg2
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
+
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -32,7 +32,7 @@ def after_request(response):
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
